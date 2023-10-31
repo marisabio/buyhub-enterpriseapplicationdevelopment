c37166709fa5454a5d0104487257e3453124074e
mari <mariana.msabio@gmail.com>
2023-10-31 18:27:46 -0300

    Integração de Spring ao projeto

--- src/main/java/org/buyhub/Main.java
+++ src/main/java/org/buyhub/Main.java
@@ -3,11 +3,16 @@ package org.buyhub;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
         var entityManagerFactory = Persistence.createEntityManagerFactory("oracle");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        SpringApplication.run(Main.class, args);
     }
 
 }
