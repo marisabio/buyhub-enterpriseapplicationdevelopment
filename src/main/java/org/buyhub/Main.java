e5f2a627d64375a4c4351938cc6da9fbb948a6b0
mari <mariana.msabio@gmail.com>
2023-11-07 11:18:14 -0300

    Alterando as configurações do Hibernate para o Spring

--- src/main/java/org/buyhub/Main.java
+++ src/main/java/org/buyhub/Main.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) throws Exception {
-        var entityManagerFactory = Persistence.createEntityManagerFactory("oracle");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
         SpringApplication.run(Main.class, args);
     }
 }
